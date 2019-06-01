a6c972b0da4e27c66a2e3070913626a13c8766ba
arufa2530 <moriken.0325@gmail.com>
2019-06-01 11:44:42 +0900

    本日分3

---
+++ THE_HELP/Assets/script/Follow.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    GameObject targetObj;
+
+    Vector3 targetPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetObj = GameObject.Find("TargetGameObject");
+        targetPos = targetObj.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Following();
+    }
+
+    private void Following()
+    {
+        //
+        transform.position += targetObj.transform.position - targetPos;
+        targetPos = targetObj.transform.position;
+
+        //
+        if(Input.GetMouseButton(1))
+        {
+            //
+            float mouseInputX = Input.GetAxis("Mouse X");
+            float mouseInputY = Input.GetAxis("Mouse Y");
+            //
+            transform.RotateAround(targetPos, Vector3.up, mouseInputX * Time.deltaTime * 200.0f);
+            //
+            transform.RotateAround(targetPos, transform.right, mouseInputY * Time.deltaTime * 200.0f);
+        }
+    }
+    
+}
---
+++ THE_HELP/Assets/script/MainCamera.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class GetMainCamera : MonoBehaviour
+{
+    float inputHorizontal;
+    float inputVertical;
+
+    Rigidbody rd;
+
+    float moveSpeed = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rd = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputHorizontal = Input.GetAxisRaw("Horizontal");
+        inputVertical = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        //
+        Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+        //
+        Vector3 moveForward = cameraForward * inputVertical + Camera.main.transform.right * inputHorizontal;
+        //
+        rd.velocity = moveForward * moveSpeed + new Vector3(0, rd.velocity.y, 0);
+        //
+        if(moveForward != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(moveForward);
+        }
+    }
+
+}
